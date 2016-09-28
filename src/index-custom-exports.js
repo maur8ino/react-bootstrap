cd8ff41bad856057ae151b8b18891b7aac65105c
Mauro Verrocchio <mauroverrocchio@gmail.com>
2016-09-28 10:59:25 +0200

    adds Tooltip and OverlayTrigger to custom exports

--- src/index-custom-exports.js
+++ src/index-custom-exports.js
@@ -1,5 +1,9 @@
+// Modal
 export Modal from './Modal';
 export ModalBody from './ModalBody';
 export ModalFooter from './ModalFooter';
 export ModalHeader from './ModalHeader';
 export ModalTitle from './ModalTitle';
+// Tooltip
+export OverlayTrigger from './OverlayTrigger';
+export Tooltip from './Tooltip';
